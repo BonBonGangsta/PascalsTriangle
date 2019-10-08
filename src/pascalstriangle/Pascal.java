262842a21025f85cc141a71978375a73791ebdde
Byron De Paz <bl3203@gmail.com>
2019-10-08 19:31:11 -0400

    additional comments added

--- src/pascalstriangle/Pascal.java
+++ src/pascalstriangle/Pascal.java
@@ -20,7 +20,7 @@ public class Pascal {
      */
     public void Pascal(int neededRows)
     {
-        int columns = 1;
+        int columns = 1; // starts at 1 since the zero is going to be constant
         pasTri = new int [neededRows][] ; // initialize the pyramid
         limitOfColRows = neededRows ; // set the limit
         
@@ -28,21 +28,27 @@ public class Pascal {
         // are just constant.
         pasTri [0][0] = 1 ; // first value is 1
         
-        // for loop with a condition
+        // for loop with a condition for the roles to run for the number of
+        // rows requested by the user
         for (int r = 1 ; r < limitOfColRows ; r++)
         {
+            // each row above row 0 is different so we must use a dynamic
+            // for loop to add the values into their respective columns
             for (int c = 0 ; c <= columns ; c++ )
             {
+                // the first and last columns get the constant value of 1
                 if ( c == columns || c == 0 )
                 {
                    pasTri [r][c] = 1;
                 }
                 else
                 {
+                    // get the sum of the previous rows, same column and
+                    // previous column
                     pasTri [r][c] = pasTri [r-1][c] + pasTri [r-1][c-1];
                 }
             }
-            columns++;
+            columns++; // add to the columns
         }
     }
    
